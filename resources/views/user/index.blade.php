e63b5ea36f5a4b8293680e9ee979429a4ed3aae1
mirzaromi <mirzaromi12@gmail.com>
2022-04-04 03:15:17 +0700

    menghapus logo dan mengganti logo

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -54,18 +54,12 @@
                         <b class="logo-icon">
                             <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                             <!-- Dark Logo icon -->
-                            <img src="/assets/user/flexy-bootstrap-lite/assets/images/logo-icon.png" alt="homepage" class="dark-logo" />
+                            <img src="/img/Logo MAI Lengkap Berwarna.png" alt="homepage" class="dark-logo" width="200px" />
                             <!-- Light Logo icon -->
                             <img src="/assets/user/flexy-bootstrap-lite/assets/images/logo-light-icon.png" alt="homepage" class="light-logo" />
                         </b>
                         <!--End Logo icon -->
                         <!-- Logo text -->
-                        <span class="logo-text">
-                            <!-- dark Logo text -->
-                            <img src="/assets/user/flexy-bootstrap-lite/assets/images/logo-text.png" alt="homepage" class="dark-logo" />
-                            <!-- Light Logo text -->
-                            <img src="/assets/user/flexy-bootstrap-lite/assets/images/logo-light-text.png" class="light-logo" alt="homepage" />
-                        </span>
                     </a>
                     <!-- ============================================================== -->
                     <!-- End Logo -->
