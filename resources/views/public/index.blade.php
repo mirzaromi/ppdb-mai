692a5c517fe2f9c4622782eafedc22f5833d9a80
Fth87 <itsthedreamgt@gmail.com>
2022-05-22 11:54:16 +0700

    perubahan2

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -29,6 +29,7 @@
                                 aria-label="Close"></button>
                         </div>
                         <ul class="navbar-nav">
+                            <li class="nav-item"><a class="nav-link scroll" href="#home">Homeee1</a></li>
                             <li class="nav-item"><a class="nav-link scroll" href="#home">Homeee</a></li>
                             <li class="nav-item"><a class="nav-link scroll" href="#home">Branda</a></li>
                             <li class="nav-item"><a class="nav-link scroll" href="#services">Tentang</a></li>
