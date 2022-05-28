6a98893c470f7c453bbd591906e0dbc64f5b2e78
Fth87 <itsthedreamgt@gmail.com>
2022-05-28 13:34:41 +0700

    contact

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -15,8 +15,8 @@
 </head>
 
 <body class="onepage" data-bs-spy="scroll" data-bs-target=".navbar">
-    <div class="content-wrapper">
-        <header class="wrapper bg-gray">
+    <div class="content-wrapper ">
+        <header class="wrapper bg-gray shadow-nav">
             <nav class="navbar navbar-expand-lg center-logo transparent navbar-light">
                 <div class="container flex-lg-row flex-nowrap align-items-center">
                     <div class="navbar-brand w-100">
@@ -96,7 +96,7 @@
         </section>
         <!-- /section -->
         <section class="" id="services">
-            <div class="wrapper bg-light jarak-section">
+            <div class="wrapper bg-light padding-xy">
                 <div class="container">
                     <div class="row  mb-1 pb-1">
                         <div class="col-lg-12 text-center">
@@ -154,7 +154,7 @@
     </section>
     <!-- /section -->
     <section class="page-section" id="jalur">
-        <div class="container jarak-section">
+        <div class="container jarak-section padding-xy">
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <h1 class="text-uppercase fw-bold font-judul">Jalur Masuk</h1>
@@ -187,7 +187,7 @@
     <!-- /section -->
     {{-- Timeline --}}
 
-    <div class="container py-5 d-flex justify-content-center">
+    <div class="container py-5 d-flex justify-content-center padding-xy">
         <div class="row">
             <div class="col-md-12">
                 <div id="content">
@@ -254,17 +254,19 @@
                     <ul class="timeline-1 text-black">
                         <li class="event" data-date="Gelombang II">
                             <h4 class="mb-0">Pendaftaran</h4>
-                                <h4 class="mb-3">10 Januari 2022 – 23 Maret 2022</h4>
-                            <p>                                
-                                Pendaftaran gelombang II dapat dilakukan secara online dan offline. Untuk pendaftaran online dapat dilakukan pada website PPDB sedangkan untuk offline dapat mendatangi langsung ke posko PPDB</p>
+                            <h4 class="mb-3">10 Januari 2022 – 23 Maret 2022</h4>
+                            <p>
+                                Pendaftaran gelombang II dapat dilakukan secara online dan offline. Untuk pendaftaran
+                                online dapat dilakukan pada website PPDB sedangkan untuk offline dapat mendatangi
+                                langsung ke posko PPDB</p>
                         </li>
                         <li class="event" data-date="26 Maret 2022">
                             <h4 class="mb-3 pt-3">Tes Akademik</h4>
                             <p>Tes Akademik dilaksanakan secara online serentak melalui mekanisme (CBT)</p>
                         </li>
                         <li class="event" data-date="03 April 2022">
-                            <h4 class="mb-3 pt-3">Pengumuman Lolos Jalur Tes  
-                                </h4>
+                            <h4 class="mb-3 pt-3">Pengumuman Lolos Jalur Tes
+                            </h4>
                             <p>Pengumuman lolos bisa dilihat di akun masing-masing peserta</p>
                         </li>
                         <li class="event" data-date="">
@@ -277,161 +279,70 @@
     </div>
     {{-- end Timeline --}}
     <!-- /section -->
-    <section id="testimonials">
-        <div class="wrapper bg-gray">
-            <div class="container py-14 py-md-17">
-                <div class="row gx-lg-8 gx-xl-12 gy-6 mb-15 align-items-center">
-                    <div class="col-lg-7 order-lg-2">
-                        <figure><img class="w-auto" src="/assets/public/assets/img/concept/concept4.png"
-                                srcset="/assets/public/assets/img/concept/concept4@2x.png 2x" alt="" /></figure>
-                    </div>
-                    <!--/column -->
-                    <div class="col-lg-5 mt-lg-12">
-                        <div class="basic-slider owl-carousel gap-small" data-margin="30">
-                            <div class="item">
-                                <blockquote class="icon icon-top fs-lg text-center">
-                                    <p>“Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-                                        Vestibulum ligula porta felis euismod semper. Cras justo odio consectetur.”
-                                    </p>
-                                    <div class="blockquote-details justify-content-center text-center">
-                                        <div class="info ps-0">
-                                            <h5 class="mb-1">Coriss Ambady</h5>
-                                            <p class="mb-0">Financial Analyst</p>
-                                        </div>
-                                    </div>
-                                </blockquote>
-                            </div>
-                            <!-- /.item -->
-                            <div class="item">
-                                <blockquote class="icon icon-top fs-lg text-center">
-                                    <p>“Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-                                        Vestibulum ligula porta felis euismod semper. Cras justo odio consectetur.”
-                                    </p>
-                                    <div class="blockquote-details justify-content-center text-center">
-                                        <div class="info ps-0">
-                                            <h5 class="mb-1">Cory Zamora</h5>
-                                            <p class="mb-0">Marketing Specialist</p>
-                                        </div>
-                                    </div>
-                                </blockquote>
-                            </div>
-                            <!-- /.item -->
-                            <div class="item">
-                                <blockquote class="icon icon-top fs-lg text-center">
-                                    <p>“Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-                                        Vestibulum ligula porta felis euismod semper. Cras justo odio consectetur.”
-                                    </p>
-                                    <div class="blockquote-details justify-content-center text-center">
-                                        <div class="info ps-0">
-                                            <h5 class="mb-1">Nikolas Brooten</h5>
-                                            <p class="mb-0">Sales Manager</p>
-                                        </div>
-                                    </div>
-                                </blockquote>
-                            </div>
-                            <!-- /.item -->
-                        </div>
-                        <!-- /.owl-carousel -->
-                    </div>
-                    <!--/column -->
-                </div>
-                <!--/.row -->
-                <div class="px-lg-5">
-                    <div class="row gx-0 gx-md-8 gx-xl-12 gy-8 align-items-center">
-                        <div class="col-4 col-md-2">
-                            <figure class="px-5 px-md-0 px-lg-2 px-xl-3 px-xxl-4"><img
-                                    src="/assets/public/assets/img/brands/c1.png" alt="" /></figure>
-                        </div>
-                        <!--/column -->
-                        <div class="col-4 col-md-2">
-                            <figure class="px-5 px-md-0 px-lg-2 px-xl-3 px-xxl-4"><img
-                                    src="/assets/public/assets/img/brands/c2.png" alt="" /></figure>
-                        </div>
-                        <!--/column -->
-                        <div class="col-4 col-md-2">
-                            <figure class="px-5 px-md-0 px-lg-2 px-xl-3 px-xxl-4"><img
-                                    src="/assets/public/assets/img/brands/c3.png" alt="" /></figure>
-                        </div>
-                        <!--/column -->
-                        <div class="col-4 col-md-2">
-                            <figure class="px-5 px-md-0 px-lg-2 px-xl-3 px-xxl-4"><img
-                                    src="/assets/public/assets/img/brands/c4.png" alt="" /></figure>
-                        </div>
-                        <!--/column -->
-                        <div class="col-4 col-md-2">
-                            <figure class="px-5 px-md-0 px-lg-2 px-xl-3 px-xxl-4"><img
-                                    src="/assets/public/assets/img/brands/c5.png" alt="" /></figure>
-                        </div>
-                        <!--/column -->
-                        <div class="col-4 col-md-2">
-                            <figure class="px-5 px-md-0 px-lg-2 px-xl-3 px-xxl-4"><img
-                                    src="/assets/public/assets/img/brands/c6.png" alt="" /></figure>
-                        </div>
-                        <!--/column -->
-                    </div>
-                    <!--/.row -->
-                </div>
-                <!-- /div -->
-            </div>
-            <!-- /.container -->
-        </div>
-        <!-- /.wrapper -->
-    </section>
-    <!-- /section -->
+
+
+    {{-- Contact Person --}}
     <section id="contact">
-        <div class="wrapper bg-light">
-            <div class="container py-14 py-md-17">
-                <div class="row gx-lg-8 gx-xl-12 gy-10 align-items-center">
-                    <div class="col-lg-7">
-                        <figure><img class="w-auto" src="/assets/public/assets/img/concept/concept5.png"
-                                srcset="/assets/public/assets/img/concept/concept5@2x.png 2x" alt="" /></figure>
-                    </div>
-                    <!--/column -->
-                    <div class="col-lg-5">
-                        <h2 class="fs-15 text-uppercase text-line text-primary text-center mb-3">Get In Touch</h2>
-                        <h3 class="display-5 mb-7">Got any questions? Don't hesitate to get in touch.</h3>
-                        <div class="d-flex flex-row">
-                            <div>
-                                <div class="icon text-primary fs-28 me-4 mt-n1"> <i
-                                        class="uil uil-location-pin-alt"></i> </div>
-                            </div>
-                            <div>
-                                <h5 class="mb-1">Address</h5>
-                                <address>Moonshine St. 14/05 Light City, London</address>
-                            </div>
-                        </div>
-                        <div class="d-flex flex-row">
-                            <div>
-                                <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-phone-volume"></i>
-                                </div>
-                            </div>
-                            <div>
-                                <h5 class="mb-1">Phone</h5>
-                                <p>00 (123) 456 78 90</p>
-                            </div>
-                        </div>
-                        <div class="d-flex flex-row">
-                            <div>
-                                <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-envelope"></i>
-                                </div>
-                            </div>
-                            <div>
-                                <h5 class="mb-1">E-mail</h5>
-                                <p class="mb-0"><a href="mailto:sandbox@email.com"
-                                        class="link-body">sandbox@email.com</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/column -->
+        <div class="container padding-xy">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h1 class="text-uppercase">contact person</h1>
+                    <p class="text-uppercase fst-italic">Narahubung yang dapat dihubungi terkait PPDB</p>
                 </div>
-                <!--/.row -->
             </div>
-            <!-- /.container -->
+
+            <div class="row">
+                <div class="col-sm-4 d-flex justify-content-center">
+                    <div class="shadow-sm p-3 mb-5 bg-body rounded">
+                        <img class="mx-auto rounded-circle shadow-img d-block" width="200px" src="/img/user.jpg" alt="">
+                        <div class="text-center margin-1">
+                            <h4>Admin 2</h4>
+                            <div>
+                                <a href="https://api.whatsapp.com/send?phone=6281216717770&" target="_blank" class="hvr-icon-grow-rotate">
+                                    <i class="fab fa-whatsapp fa-3x hvr-icon"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              
+                <div class="col-sm-4 d-flex justify-content-center">
+                    <div class="shadow-sm p-3 mb-5 bg-body rounded">
+                        <img class="mx-auto rounded-circle shadow-img d-block" width="200px" src="/img/user.jpg" alt="">
+                        <div class="text-center margin-1">
+                            <h4>Admin 3</h4>
+                            <div>
+                                <a href="https://api.whatsapp.com/send?phone=6285817231044&" target="_blank" class="hvr-icon-grow-rotate">
+                                    <i class="fab fa-whatsapp fa-3x hvr-icon"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              
+                <div class="col-sm-4 d-flex justify-content-center">
+                    <div class="shadow-sm p-3 mb-5 bg-body rounded">
+                        <img class="mx-auto rounded-circle shadow-img d-block" width="200px" src="/img/user.jpg" alt="">
+                        <div class="text-center margin-1">
+                            <h4>Admin 1</h4>
+                            <div>
+                                <a href="https://api.whatsapp.com/send?phone=6285157748815&amp;" target="_blank" class="hvr-icon-grow-rotate">
+                                    <i class="fab fa-whatsapp fa-3x hvr-icon"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              
+
+
+            </div>
         </div>
-        <!-- /.wrapper -->
     </section>
     <!-- /section -->
-    </div>
+
+
+
     <!-- /.content-wrapper -->
     <footer class="bg-dark text-inverse">
         <div class="container py-13 py-md-15">
